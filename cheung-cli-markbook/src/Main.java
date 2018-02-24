ce929f01fe81d1902c8c2db4f6a8b187d26dfe85
Abby cheung <abby.cheung18@ycdsbk12.ca>
2018-02-23 21:46:27 -0500

    update student input

--- cheung-cli-markbook/src/Main.java
+++ cheung-cli-markbook/src/Main.java
@@ -6,31 +6,44 @@ import java.util.Scanner;
 public class Main {
 
     public static void inputStudent() {
-        Scanner input = new Scanner(System.in);
-
-        ArrayList firstName = new ArrayList();
-        ArrayList lastName = new ArrayList();
-        ArrayList studentNumber = new ArrayList();
-        ArrayList gradYear = new ArrayList();
-
-        System.out.println("Student first name?");
-
-        if (input.next() == "NONe") {
-
-        }
-        firstName.add(input.next());
-
-        System.out.println("Student last name?");
-        lastName.add(input.next());
-
-
-
-
+       
     }
 
     public static void main(String[] args) {
-        inputStudent();
-
+    	 Scanner input = new Scanner(System.in);
+ 
+         ArrayList<String> firstName = new ArrayList<String>();
+         ArrayList<String> lastName = new ArrayList<String>();
+         ArrayList<Integer> studentNumber = new ArrayList<Integer>();
+         ArrayList<Integer> gradYear = new ArrayList<Integer>();
+         
+         System.out.println("How many students do you want to input? ");
+         int students = input.nextInt();
+         
+         for (int i = 0; i <= students; i++) {
+ 	        System.out.println("Student first name?");
+ 	    	String firstN = input.next();
+ 	        
+ 	        if (firstN.equals("none")) {
+ 	        	break;
+ 	        } else {
+ 	        	firstName.add(firstN);
+ 	        	
+ 	        	System.out.println("Student last name?");
+ 		        lastName.add(input.next());
+ 		        
+ 		        System.out.println("Student number?");
+ 		        studentNumber.add(input.nextInt());
+ 		        
+ 		        System.out.println("Student graduation year?");
+ 		        gradYear.add(input.nextInt());
+ 	        }	
+         }
+        
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(studentNumber);
+        System.out.println(gradYear);
     }
 
 }
