d759b14bce71f59a46543bb7ed343fdaf8167e4b
Abby cheung <abby.cheung18@ycdsbk12.ca>
2018-02-23 22:26:01 -0500

    update, add exit

--- cheung-cli-markbook/src/Main.java
+++ cheung-cli-markbook/src/Main.java
@@ -14,41 +14,46 @@ public class Main {
         ArrayList<Integer> studentNumber = new ArrayList<Integer>();
         ArrayList<Integer> gradYear = new ArrayList<Integer>();
          
-        System.out.println("What do you want to do? \n inStudent - add student info \n view - view all student info");
-        String response = input.next();
-         
-         
-        if (response.equals("inStudent")) {
-        	System.out.println("How many students do you want to input? ");
-            int students = input.nextInt();
+        for (;;) {
+        	System.out.println("What do you want to do? \n inStudent - add student info \n view - view all student info \n exit");
+            String response = input.next();
              
-            studentInfo:
-            for (int i = 1; i <= students; i++) {
-     	       System.out.println("Student first name?");
-     	       String firstN = input.next();
-     	        
-     	       if (firstN.equals("none")) {
-     	    	   break studentInfo;
-     	       } else {
-     	    	   firstName.add(firstN);
-     	    	   System.out.println("Student last name?");
-     	    	   lastName.add(input.next());
-     	    	   
-     	    	   System.out.println("Student number?");
-     	    	   studentNumber.add(input.nextInt());
-     	    	   System.out.println("Student graduation year?");
-     	    	   gradYear.add(input.nextInt());
-     	        }	
+             
+            if (response.equals("inStudent")) {
+            	System.out.println("How many students do you want to input? ");
+                int students = input.nextInt();
+                 
+                studentInfo:
+                for (int i = 1; i <= students; i++) {
+         	       System.out.println("Student first name?");
+         	       String firstN = input.next();
+         	        
+         	       if (firstN.equals("none")) {
+         	    	   break studentInfo;
+         	       } else {
+         	    	   firstName.add(firstN);
+         	    	   System.out.println("Student last name?");
+         	    	   lastName.add(input.next());
+         	    	   
+         	    	   System.out.println("Student number?");
+         	    	   studentNumber.add(input.nextInt());
+         	    	   System.out.println("Student graduation year?");
+         	    	   gradYear.add(input.nextInt());
+         	        }	
+                 }
              }
-         }
-        
-         if (response.equals("view")) {
-        	 System.out.println(firstName);
-        	 System.out.println(lastName);
-        	 System.out.println(studentNumber);
-        	 System.out.println(gradYear);
-         }
-        
+            
+             if (response.equals("view")) {
+            	 System.out.println(firstName);
+            	 System.out.println(lastName);
+            	 System.out.println(studentNumber);
+            	 System.out.println(gradYear);
+             }
+             
+             if (response.equals("exit")) {
+            	 System.exit(0);
+             }
+        }   
     }
 
 }
