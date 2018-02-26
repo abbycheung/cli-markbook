a16da46db03d6d0c2b188900747849bb8404e87c
parisngow <36411103+parisngow@users.noreply.github.com>
2018-02-26 09:16:15 -0500

    Update Main.java
    add google login feature

--- cheung-cli-markbook/src/Main.java
+++ cheung-cli-markbook/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         int[] marks = new int[]{10,20,15,25,16,60,100};
          
         for (;;) {
-        	System.out.println("What do you want to do? \n inStudent - add student info \n view - view all student info \n average - calculate class average \n exit");
+        	System.out.println("What do you want to do? \n inStudent - add student info \n view - view all student info \n average - calculate class average \n Login - retrieve Google login \n exit");
             String response = input.next();
              
              
@@ -66,6 +66,29 @@ public class Main {
              if (response.equals("exit")) {
             	 System.exit(0);
              }
+            
+            if (response.equals("login")) {
+                System.out.println("Please enter the following information");
+                
+                //retrieving user information for google login
+                System.out.print("Student's first name: ");
+                String firstName = input.next();
+                System.out.print("Student's last name: ");
+                String lastName = input.next();
+                System.out.print("Student number:");
+                String studentNumber = input.next();
+                System.out.print("Student's graduation year:");
+                String gradYear = input.next();
+
+                System.out.println(" ");
+
+                //putting together google login
+                String email = firstName + "." + lastName + gradYear.substring(2, 4) + "@ycdsbk12.ca";
+                String password = firstName.substring(0, 1).toUpperCase() + lastName.substring(0, 1).toLowerCase() + studentNumber;
+
+                System.out.println("Email: " + email);
+                System.out.println("Password: " + password);
+            }
         }   
     }
 
